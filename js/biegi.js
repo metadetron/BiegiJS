32e44f7173d9b07fcbba987b14f2e710727da818
METADETRON <contact@metadetron.com>
2016-09-24 23:15:56 +0200

    alkocommit

--- js/biegi.js
+++ js/biegi.js
@@ -1,3 +1,19 @@
+////////////////////////////// M O D E L S ////////////////////////////////////
+
+  var Month = Backbone.Model.extend({
+    defaults: {
+      date: null,
+      distance: 0
+    }
+  });
+
+  var Months = Backbone.Collection.extend({
+    model: Month,
+    url:"http://run.metadetron.com/Biegi/miesiac/"
+  });
+
+////////////////////////////// V I E W S /////////////////////////////////
+
 // co tu sie dzieje sie?
 (function($){
     // definicja widoku
@@ -42,64 +58,61 @@
                 var data = new google.visualization.DataTable();
                 data.addColumn('timeofday', 'Time of Day');
                 data.addColumn('number', 'Motivation Level');
-
-                data.addRows([
-                    [{v: [8, 0, 0], f: '8 am'}, 1],
-                    [{v: [9, 0, 0], f: '9 am'}, 2],
-                    [{v: [10, 0, 0], f:'10 am'}, 3],
-                    [{v: [11, 0, 0], f: '11 am'}, 4],
-                    [{v: [12, 0, 0], f: '12 pm'}, 5],
-                    [{v: [13, 0, 0], f: '1 pm'}, 6],
-                    [{v: [14, 0, 0], f: '2 pm'}, 7],
-                    [{v: [15, 0, 0], f: '3 pm'}, 8],
-                    [{v: [16, 0, 0], f: '4 pm'}, 9],
-                    [{v: [17, 0, 0], f: '5 pm'}, 10],
-                ]);
-
                 var options = {
                     title: 'Motivation and Energy Level Throughout the Day',
                     focusTarget: 'category',
                     hAxis: {
-                    title: 'Time of Day',
-                    format: 'h:mm a',
-                    viewWindow: {
-                        min: [7, 30, 0],
-                        max: [17, 30, 0]
-                    },
-                    textStyle: {
-                        fontSize: 14,
-                        color: '#053061',
-                        bold: true,
-                        italic: false
-                    },
-                    titleTextStyle: {
-                        fontSize: 18,
-                        color: '#053061',
-                        bold: true,
-                        italic: false
-                    }
+                        title: 'Time of Day',
+                        format: 'h:mm a',
+                        viewWindow: {
+                            min: [7, 30, 0],
+                            max: [17, 30, 0]
+                        },
+                        textStyle: {
+                            fontSize: 14,
+                            color: '#053061',
+                            bold: true,
+                            italic: false
+                        },
+                        titleTextStyle: {
+                            fontSize: 18,
+                            color: '#053061',
+                            bold: true,
+                            italic: false
+                        }
                     },
                     vAxis: {
-                    title: 'Rating (scale of 1-10)',
-                    textStyle: {
-                        fontSize: 18,
-                        color: '#67001f',
-                        bold: false,
-                        italic: false
-                    },
-                    titleTextStyle: {
-                        fontSize: 18,
-                        color: '#67001f',
-                        bold: true,
-                        italic: false
-                    }
+                        title: 'Rating (scale of 1-10)',
+                        textStyle: {
+                            fontSize: 18,
+                            color: '#67001f',
+                            bold: false,
+                            italic: false
+                        },
+                        titleTextStyle: {
+                            fontSize: 18,
+                            color: '#67001f',
+                            bold: true,
+                            italic: false
+                        }
                     }
                 };
-
-                var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-                chart.draw(data, options);
-                }
-            
+                // wywolaj api pobierajace liste danych
+                var months = new Months();
+                months.fetch(
+                    {
+                        success: function() {            
+                            data.addRows(months);
+                            view.render();                
+                            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+                            chart.draw(data, options);
+                        },
+                        error: function(model, response) {            
+                            alert(response.statusText);
+                        }
+                    }
+                );
+            }            
         }
     });
     new ChartView();
