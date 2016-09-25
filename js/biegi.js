5c761027459afac8a4e097c87f38cdf24759c230
METADETRON <contact@metadetron.com>
2016-09-25 09:33:13 +0200

    bez fejka

--- js/biegi.js
+++ js/biegi.js
@@ -1,29 +1,6 @@
 ////////////////////////////// M O D E L S ////////////////////////////////////
 
 ////////////////////////////// V I E W S /////////////////////////////////
-
-// co tu sie dzieje sie?
-(function($){
-    // definicja widoku
-    var LorumView = Backbone.View.extend({
-        el: $('#col_left'), // renderowanego w tym elemencie
-        initialize: function(){
-            _.bindAll(this, 'render'); // zeby metody znaly "this" 
-            this.render(); // samorenderujacego sie na starcie 
-        },
-        render: function(){
-            var that = this;
-            $.get('tpl/portlet.html', 
-                function(data) {
-                    $(that.el).append( _.template(data));
-                }, 
-                'html'
-            );
-        }
-    });
-    new LorumView();
-})(jQuery);
-
 (function($){
     var ChartView = Backbone.View.extend({
         el: $('#col_middle'), 
