35691fd0d1ce3fa605d3ab4febaf906105193b3a
METADETRON <contact@metadetron.com>
2016-10-05 00:26:32 +0200

    onSignIn not part of biegiApp

--- js/biegi.js
+++ js/biegi.js
@@ -2,6 +2,25 @@
 
 var biegiApp = null;
 
+var onSignIn = function (googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    $("#profilePhoto").attr("src", profile.getImageUrl());
+    $("#fullName").text(profile.getName());
+    
+/*    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); 
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token); */
+};
+
+
 $.ajax({
     url: "http://run.metadetron.com/Biegi/auth"
 }).then(function(data) {    
@@ -164,27 +183,7 @@ $.ajax({
             }
         );
 
-        ///////////////////////// UTILS ///////////////////////////
-        var onSignIn = function (googleUser) {
-            // Useful data for your client-side scripts:
-            var profile = googleUser.getBasicProfile();
-            $("#profilePhoto").attr("src", profile.getImageUrl());
-            $("#fullName").text(profile.getName());
-            
-        /*    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log("Image URL: " + profile.getImageUrl());
-            console.log("Email: " + profile.getEmail()); 
-
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token); */
-        };
-
         return {
-            onSignIn: onSignIn
         };
     })(jQuery);
 }, function(data) {
