014f67f26abe865390749c6a2247a09e506e0ce6
METADETRON <contact@metadetron.com>
2016-10-05 17:23:42 +0200

    location.reload()

--- js/biegi.js
+++ js/biegi.js
@@ -1,5 +1,25 @@
 // UWAGA! Ta wersja jeszcze nie keszuje templateow!
 
+function onSignIn(googleUser) {
+    location.reload();
+    // Useful data for your client-side scripts:
+//    var profile = googleUser.getBasicProfile();
+//    $("#profilePhoto").attr("src", profile.getImageUrl());
+//    $("#fullName").text(profile.getName());
+    
+/*    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); 
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token); */
+};
+
+
 $.ajax({
     url: "http://run.metadetron.com/Biegi/auth"
 }).then(function(data) {    
@@ -162,27 +182,7 @@ $.ajax({
             }
         );
 
-        ///////////////////////// UTILS ///////////////////////////
-        function onSignIn(googleUser) {
-            // Useful data for your client-side scripts:
-            var profile = googleUser.getBasicProfile();
-            $("#profilePhoto").attr("src", profile.getImageUrl());
-            $("#fullName").text(profile.getName());
-            
-        /*    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log("Image URL: " + profile.getImageUrl());
-            console.log("Email: " + profile.getEmail()); 
-
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token); */
-        };
-
         return {
-            onSignIn: onSignIn
         };
     })(jQuery);
 }, function(data) {
